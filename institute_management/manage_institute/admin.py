f703197429cce67959f64eb28f78fee92754d3a0
Sanketmajithiya <Sanketmajithiya@gmail.com>
2024-04-30 14:59:33 +0530

    institute management

---
+++ institute_management/manage_institute/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Student, Teacher, Department
+
+# Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Department)
+# admin.site.register(Book)
+
---
+++ institute_management/manage_institute/urls.py
@@ -0,0 +1,80 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.home, name='home'),
+   #  path('', views.hom, name='home'),
+    
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.urls import path
+# from.views import *
+# urlpatterns = [
+#      path('register/',register_view,name='register'),
+#      path('login/',login_view,name='login'),
+#     #
+     
+#  ]
+ 
