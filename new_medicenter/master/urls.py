69765e136f1bd6765a8e876fc3ada418628ddc5c
kimu7466 <kimu7466@gmail.com>
2024-05-19 13:24:57 +0530

    #

---
+++ new_medicenter/master/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('dashboard_view/', dashboard_view, name='dashboard_view'),
+    path('doctors_view/', doctors_view, name='doctors_view'),
+    path('patients_view/', patients_view, name='patients_view')
+]
